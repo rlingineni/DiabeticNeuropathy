0c26de029981c7ce7c1b66910240c18fdb673d44
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2015-05-27 20:52:25 -0500

    simple testing webpage. lists off all data pulled

---
+++ JS/data.js
@@ -0,0 +1,26 @@
+function fetchData(){
+    fetchLeft();
+    fetchRight();
+    writeData();
+}
+
+function fetchLeft(){    $.getJSON("https://api.particle.io/v1/devices/54ff71066672524822431867/fsrLeft?access_token=eef5a0cba3f7e74a20df3a6d9b49229ce8b54fc7", function(data) {
+
+        window.dataL = data.result
+    });
+}
+
+function fetchRight(){     $.getJSON("https://api.particle.io/v1/devices/54ff71066672524822431867/fsrRight?access_token=eef5a0cba3f7e74a20df3a6d9b49229ce8b54fc7", function(data) {
+
+        window.dataR = data.result
+    });
+}
+
+function writeData(data){
+    dataL = window.dataL;
+    dataR = window.dataR;
+    $( ".content" ).append( "<p>" + dataL + ", " + dataR + "</p>" );
+}
+
+
+setInterval(fetchData, 500);
